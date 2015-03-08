57567e6aca0a0bd9c126504bc0b8a38affff8d47
Jordi Collell <jordic@gmail.com>
2015-03-08 12:27:33 +0100

    New encoder api

--- README.md
+++ README.md
@@ -15,19 +15,18 @@ http://tools.ietf.org/html/rfc5545
 
 
 TODO
-Reflect:
-https://gist.github.com/drewolson/4771479
+--
 
-Tests from:
+Integrate testing from:
 https://github.com/libical/libical
 
-Decoder` that you create by passing it an `io.Reader`, similar to your setup
-Joe Shaw [9:37 PM]
-and then you `err := dec.Decode(&foo)`, where `foo` for `json` is `interface{}` but 
 
-also curious why `WriteCalendar` and `WriteEvent` aren't methods of `*Writer`
+CHANGELOG:
+--
 
-Joe Shaw [8:10 PM]
-seems like you'd want `WriteLine` and a lot of the helper functions to be unexported
+- v00. First api traial
+- v01. Api evolves to a ical toolkit
 
-i think returning an unexported struct from an exported function is considered a no-no, which you do with `func NewDecoder(r io.Reader) *decoder`
+Thanks to:
+Joe Shaw Reviewing first revision.
+
--- encoder.go
+++ encoder.go
@@ -2,8 +2,8 @@ package goics
 
 import (
 	"io"
-	"strings"
 	"time"
+	"strings"
 )
 
 const (
@@ -11,114 +11,114 @@ const (
 	CRLFSP = "\r\n "
 )
 
-type Writer struct {
-	w    io.Writer
-	opts map[string]func(string, time.Time) string
+
+
+func NewComponent() *Component {
+	return &Component{
+		Elements:   make([]Componenter, 0),
+		Properties: make(map[string]string),
+	}
+}
+
+type Component struct {
+	Tipo       string
+	Elements   []Componenter
+	Properties map[string]string
+}
+
+func (c *Component) Write(w *ICalEncode) {
+	w.WriteLine("BEGIN:" + c.Tipo + CRLF )
+	// Iterate over component properites
+	for key, val := range c.Properties {
+		w.WriteLine( WriteStringField(key, val) )
+	
+	}
+	for _, xc := range c.Elements {
+		xc.Write( w )	
+	}
+	
+	w.WriteLine("END:" + c.Tipo + CRLF)
+}
+
+func (c *Component) SetType(t string) {
+	c.Tipo = t
+}
+
+func (c *Component) AddComponent(cc Componenter) {
+	c.Elements = append(c.Elements, cc)
+}
+
+func (c *Component) AddProperty(key string, val string) {
+	c.Properties[key] = val
 }
 
 
-var defaultOptions = map[string]func(string, time.Time) string{
-	"Start":        WriteDateField,
-	"End":          WriteDateField,
-	"LastModified": WriteDateTimeField,
-	"Dtstamp":      WriteDateTimeField,
-	"Created":      WriteDateTimeField,
+// ICalEncode is the real writer, that wraps every line, 
+// in 75 chars length... Also gets the component from the emmiter
+// and starts the iteration.
+
+type ICalEncode struct {
+	w io.Writer
+}
+
+func NewICalEncode(w io.Writer) *ICalEncode {
+	return &ICalEncode{
+		w: w,
+	}
+}
+
+func (enc *ICalEncode) Encode(c ICalEmiter) {
+	component := c.EmitICal()
+	component.Write(enc)
 }
 
 var LineSize int = 75
 
-// Creates a New writer with default options for date fields
-func NewWriter(w io.Writer) *Writer {
-	return &Writer{
-		w:    w,
-		opts: defaultOptions,
+// Write a line in ics format max length = 75
+// continuation lines start with a space.
+func(enc *ICalEncode)  WriteLine(s string) {
+	if len(s) <= LineSize {
+		io.WriteString(enc.w, s)
+		return
 	}
-}
-
-// Creates a new Writer with custom options for Date Fields
-func NewWriterOpts(w io.Writer, opts map[string]func(string, time.Time) string) *Writer {
-	return &Writer{
-		w:    w,
-		opts: opts,
+	length := len(s)
+	current := 0
+	// LineSize -2 is CRLF
+	shortLine := LineSize - 2
+	// First line write from 0 to totalline - 2 ( must include CRLFS)
+	io.WriteString(enc.w, s[current:current+(shortLine)]+CRLFSP)
+	current = shortLine
+	// Rest of lines, we must include ^space at begining for marquing
+	// continuation lines
+	for (current + shortLine) <= length {
+		io.WriteString(enc.w, s[current:current+(shortLine-1)]+CRLFSP)
+		current += shortLine - 1
 	}
-}
-
-// Writes a calendar to writer.
-func WriteCalendar(c *Calendar, w *Writer) {
-	w.WriteLine("BEGIN:VCALENDAR" + CRLF)
-	if c.Prodid != "" {
-		w.WriteLine(WriteStringField("Prodid", c.Prodid))
-	} else {
-		w.WriteLine(WriteStringField("Prodid", "-//tmpo.io/src/goics"))
-	}
-	if c.Calscale != "" {
-		w.WriteLine(WriteStringField("Calscale", c.Calscale))
-	} else {
-		w.WriteLine(WriteStringField("Calscale", "GREGORIAN"))
-	}
-	if c.Version != "" {
-		w.WriteLine(WriteStringField("Version", c.Version))
-	} else {
-		w.WriteLine(WriteStringField("Version", "2.0"))
-	}
-	if c.Uid != "" {
-		w.WriteLine(WriteStringField("Uid", c.Uid))
-	}
-	for key, val := range c.Params {
-		w.WriteLine(WriteStringField(key, val))
-	}
-	
-	for _, ev := range c.Events {
-		WriteEvent(ev, w)
-	}
-	
-	w.WriteLine("END:VCALENDAR" + CRLF)
+	// Also we need to write the reminder
+	io.WriteString(enc.w, s[current:length])
 }
 
 
 
-// Writes an event struct to a writer
-func WriteEvent(ev *Event, w *Writer) {
 
-	w.WriteLine("BEGIN:VEVENT" + CRLF)
-	// Date fields
-	t := time.Time{}
-	
-	if ev.End != t {
-		w.WriteLine(w.opts["End"]("DTEND", ev.End))
-	}
-	if ev.Start != t {
-		w.WriteLine(w.opts["Start"]("DTSTART", ev.Start))
-	}
-	if ev.LastModified != t {
-		w.WriteLine(w.opts["LastModified"]("LAST-MODIFIED", ev.LastModified))
-	}
-	if ev.Dtstamp != t {
-		w.WriteLine(w.opts["Dtstamp"]("DTSTAMP", ev.Dtstamp))
-	}
-	if ev.Created != t {
-		w.WriteLine(w.opts["Created"]("CREATED", ev.Created))
-	}
-	// String fields
-	if ev.Uid != "" {
-		w.WriteLine(WriteStringField("Uid", ev.Uid))
-	}
-	if ev.Summary != "" {
-		w.WriteLine(WriteStringField("Summary", ev.Summary))
-	}
-	if ev.Description != "" {
-		w.WriteLine(WriteStringField("Description", ev.Description))
-	}
-	if ev.Location != "" {
-		w.WriteLine(WriteStringField("Location", ev.Location))
-	}
-	if ev.Status != "" {
-		w.WriteLine(WriteStringField("Status", ev.Status))
-	}
-	if ev.Transp != "" {
-		w.WriteLine(WriteStringField("Transp", ev.Transp))
-	}
-	w.WriteLine("END:VEVENT" + CRLF)
+// Writes a Date in format: "DTEND;VALUE=DATE:20140406"
+func FormatDateField(key string, val time.Time) (string, string) {
+	return key + ";VALUE=DATE", val.Format("20060102")
+}
+
+// "X-MYDATETIME;VALUE=DATE-TIME:20120901T130000"
+func FormatDateTimeField(key string, val time.Time) (string, string) {
+	return key + ";VALUE=DATE-TIME", val.Format("20060102T150405")
+}
+
+// "DTSTART:19980119T070000Z"
+func FormatDateTime(key string, val time.Time) (string, string) {
+	return key, val.Format("20060102T150405Z") 
+}
+
+// Write a key field UID:asdfasdfаs@dfasdf.com
+func WriteStringField(key string, val string) string {
+	return strings.ToUpper(key) + ":" + quoteString(val) + CRLF
 }
 
 func quoteString(s string) string {
@@ -127,48 +127,4 @@ func quoteString(s string) string {
 	s = strings.Replace(s, "\\n", "\n", -1)
 	s = strings.Replace(s, "\\\\", "\\", -1)
 	return s
-}
-
-// Write a line in ics format max length = 75
-// continuation lines start with a space.
-func (w *Writer) WriteLine(s string) {
-	if len(s) <= LineSize {
-		io.WriteString(w.w, s)
-		return
-	}
-	length := len(s)
-	current := 0
-	// LineSize -2 is CRLF
-	shortLine := LineSize - 2
-	// First line write from 0 to totalline - 2 ( must include CRLFS)
-	io.WriteString(w.w, s[current:current+(shortLine)]+CRLFSP)
-	current = shortLine
-	// Rest of lines, we must include ^space at begining for marquing
-	// continuation lines
-	for (current + shortLine) <= length {
-		io.WriteString(w.w, s[current:current+(shortLine-1)]+CRLFSP)
-		current += shortLine - 1
-	}
-	// Also we need to write the reminder
-	io.WriteString(w.w, s[current:length])
-}
-
-// Writes a Date in format: "DTEND;VALUE=DATE:20140406"
-func WriteDateField(key string, val time.Time) string {
-	return key + ";VALUE=DATE:" + val.Format("20060102") + CRLF
-}
-
-// "X-MYDATETIME;VALUE=DATE-TIME:20120901T130000"
-func WriteDateTimeField(key string, val time.Time) string {
-	return key + ";VALUE=DATE-TIME:" + val.Format("20060102T150405") + CRLF
-}
-
-// "DTSTART:19980119T070000Z"
-func WriteDateTime(key string, val time.Time) string {
-	return key + ":" + val.Format("20060102T150405Z") + CRLF
-}
-
-// Write a key field UID:asdfasdfаs@dfasdf.com
-func WriteStringField(key string, val string) string {
-	return strings.ToUpper(key) + ":" + quoteString(val) + CRLF
 }
